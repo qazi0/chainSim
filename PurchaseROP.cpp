3d719819c1b2e2f5c720c57687209b630473b8e3
Siraj Qazi <siraj.qazi@seeloz.com>
2023-11-05 17:13:29 +0500

    Added initial PurchaseROP def. and impl.

---
+++ PurchaseROP.cpp
@@ -0,0 +1,22 @@
+//
+// Created by blakberrisigma on 03/11/2023.
+//
+
+#include "PurchaseROP.h"
+
+PurchaseROP::PurchaseROP(unsigned leadTime, double avgDemand) :
+        m_lead_time{leadTime}, m_average_daily_demand{avgDemand} {
+
+    m_safety_stock = std::ceil(m_average_daily_demand) * leadTime;
+    m_reorder_point = static_cast<long long>(m_average_daily_demand * leadTime + m_safety_stock);
+}
+
+long PurchaseROP::get_purchase(const std::unordered_map<std::string, std::vector<long>> &pastRecords,
+                                    unsigned current_day) const {
+    auto current_inventory = pastRecords.at("inventory_quantity")[current_day];
+    long reorder_quantity{0};
+    if (current_inventory <= m_reorder_point)
+        reorder_quantity = std::ceil(m_average_daily_demand * m_lead_time);
+
+    return reorder_quantity;
+}
---
+++ PurchaseROP.h
@@ -0,0 +1,26 @@
+//
+// Created by blakberrisigma on 03/11/2023.
+//
+
+#ifndef CHAINSIM_PURCHASEROP_H
+#define CHAINSIM_PURCHASEROP_H
+
+#include "PurchaseMethod.h"
+
+class PurchaseROP : public PurchaseMethod {
+    unsigned m_lead_time;
+    double m_average_daily_demand;
+    double m_safety_stock;
+    long m_reorder_point;
+
+public:
+    PurchaseROP(unsigned leadTime, double avgDemand);
+
+    [[nodiscard]] long
+    get_purchase(const std::unordered_map<std::string, std::vector<long>> &pastRecords,
+                 unsigned current_day) const;
+
+};
+
+
+#endif //CHAINSIM_PURCHASEROP_H
