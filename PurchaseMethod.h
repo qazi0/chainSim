5264abb70266f59a441fa605d60ac03a4918ce2a
Siraj Qazi <siraj.qazi@seeloz.com>
2023-11-05 17:12:52 +0500

    Added initial PurchaseMethod base header

---
+++ PurchaseMethod.h
@@ -0,0 +1,22 @@
+//
+// Created by blakberrisigma on 03/11/2023.
+//
+
+#ifndef CHAINSIM_PURCHASEMETHOD_H
+#define CHAINSIM_PURCHASEMETHOD_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class PurchaseMethod {
+public:
+    PurchaseMethod() = default;
+
+    [[nodiscard]] virtual long
+    get_purchase(const std::unordered_map<std::string, std::vector<long>>& pastRecords, unsigned current_day) const = 0;
+
+};
+
+
+#endif //CHAINSIM_PURCHASEMETHOD_H
