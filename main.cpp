fec781d3d9ab768a5ebf2c9e58b458034d6c3832
Siraj Qazi <siraj.qazi@seeloz.com>
2023-11-07 03:38:51 +0500

    use CLI

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 #include "ChainSimBuilder.h"
 #include "purchase_policies/PurchaseROP.h"
+#include "utils/CLI.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
+    argparse::ArgumentParser parser("ChainSim", "0.1");
+    qz::parse_command_line_args(parser, argc, argv);
 
-    unsigned simulationLength = 365;
-    unsigned leadTime = 8;
-    unsigned demand = 50;
-    unsigned startingInventory = 400;
+    auto log_level = parser.get<unsigned>("log_level");
+    auto simulationLength = parser.get<uint64_t>("simulation_length");
+    auto leadTime = parser.get<uint64_t>("average_lead_time");
+    auto demand = parser.get<double>("average_demand");
+    auto startingInventory = parser.get<uint64_t>("starting_inventory");
 
-    ChainSim chainSimulator = (
-            ChainSimBuilder("ChainSim")
+    qz::ChainSim chainSimulator = (
+            qz::ChainSimBuilder("ChainSim")
                     .simulation_length(simulationLength)
                     .lead_time(leadTime)
                     .average_demand(demand)
                     .starting_inventory(startingInventory)
-                    .logging_level(1)
+                    .logging_level(log_level)
                     .build()
     );
 
