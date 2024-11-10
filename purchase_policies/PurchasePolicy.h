7c4b4ab593eb5ae623e37f84e9bd06c27ca61229
Siraj Qazi <siraj.qazi@seeloz.com>
2024-11-10 18:03:01 +0500

    feat(PurchasePolicy.h): add new methods to PurchasePolicy interface for calculation details and error handling

--- purchase_policies/PurchasePolicy.h
+++ purchase_policies/PurchasePolicy.h
@@ -1,12 +1,15 @@
-
 #ifndef CHAINSIM_PURCHASEPOLICY_H
 #define CHAINSIM_PURCHASEPOLICY_H
 
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <cmath>
+#include <sstream>
+#include <stdexcept>
 
-class PurchasePolicy {
+class PurchasePolicy
+{
 public:
     using simulation_records_t = std::unordered_map<std::string, std::vector<long>>;
 
@@ -16,7 +19,10 @@ public:
     get_purchase(const simulation_records_t &pastRecords, unsigned current_day) const = 0;
 
     [[nodiscard]] virtual std::string name() const = 0;
+
+    [[nodiscard]] virtual std::string get_calculation_details(
+        const simulation_records_t &pastRecords,
+        unsigned current_day) const = 0;
 };
 
-
-#endif //CHAINSIM_PURCHASEPOLICY_H
+#endif // CHAINSIM_PURCHASEPOLICY_H
