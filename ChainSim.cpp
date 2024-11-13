cbafeea8677ac0b2c31e56fd661717fca77ea190
Siraj Qazi <siraj.qazi@seeloz.com>
2024-11-13 19:46:49 +0500

    fix output formatting

--- ChainSim.cpp
+++ ChainSim.cpp
@@ -3,12 +3,12 @@
 qz::ChainSim::ChainSim() : QObject()
 {
     m_records_columns = {
-                         QStringLiteral("inventory_quantity"),
-                         QStringLiteral("demand_quantity"),
-                         QStringLiteral("procurement_quantity"),
-                         QStringLiteral("purchase_quantity"),
-                         QStringLiteral("sale_quantity"),
-                         QStringLiteral("lost_sale_quantity")};
+        QStringLiteral("inventory_quantity"),
+        QStringLiteral("demand_quantity"),
+        QStringLiteral("procurement_quantity"),
+        QStringLiteral("purchase_quantity"),
+        QStringLiteral("sale_quantity"),
+        QStringLiteral("lost_sale_quantity")};
 }
 
 void qz::ChainSim::initialize_simulation()
@@ -30,7 +30,7 @@ void qz::ChainSim::initialize_simulation()
 
 void qz::ChainSim::simulate(const PurchasePolicy &purchasePolicy)
 {
-    m_logger.info(QStringLiteral("Starting simulation {{%1}} ...\n").arg(m_simulation_name));
+    m_logger.info(QString("Starting simulation {{%1}} ...").arg(m_simulation_name));
 
     while (m_current_day < m_simulation_length)
     {
@@ -62,23 +62,54 @@ void qz::ChainSim::simulate_days(const PurchasePolicy &purchasePolicy, quint64 d
 
 void qz::ChainSim::simulate_day(const PurchasePolicy &purchasePolicy, quint64 day)
 {
-    m_logger.info(QStringLiteral("Day# %1").arg(day));
+    // Setup field widths for consistent formatting
+    const int leftMargin = 3;    // Left margin space
+    const int labelWidth = 22;   // Width for labels
+    const int valueWidth = 6;   // Width for values
+    const int columnSpacing = 4; // Space between columns
+
+    // Helper lambda for formatting a row with three columns
+    auto formatRow = [=](const QString &label1, qint64 value1,
+                         const QString &label2, qint64 value2,
+                         const QString &label3, qint64 value3) -> QString
+    {
+        QString row;
+        QTextStream ts(&row);
+
+        // First column
+        ts << QString(leftMargin, ' ')
+           << qSetFieldWidth(labelWidth) << Qt::left << label1
+           << qSetFieldWidth(valueWidth) << value1;
+
+        // Second column
+        ts << QString(columnSpacing, ' ')
+           << qSetFieldWidth(labelWidth) << Qt::left << label2
+           << qSetFieldWidth(valueWidth) << value2;
+
+        // Third column
+        ts << QString(columnSpacing, ' ')
+           << qSetFieldWidth(labelWidth) << Qt::left << label3
+           << qSetFieldWidth(valueWidth) << value3;
+
+        return row;
+    };
+
+    // Day header
+    m_logger.info(QString(80, '-'));
+    m_logger.info(QString("%1Day #%2").arg(QString(leftMargin, ' ')).arg(day, 4, 10, QLatin1Char('0')));
 
     auto current_inventory = m_records[QStringLiteral("inventory_quantity")][day - 1];
     auto current_demand = m_records[QStringLiteral("demand_quantity")][day];
     auto current_procurement = m_records[QStringLiteral("procurement_quantity")][day];
 
-    m_logger.info(QString("%1%2%3%4%5%6")
-                      .arg(QString(10, ' ') + QString("Starting inventory: "))
-                      .arg(current_inventory)
-                      .arg(QString(15, ' ') + QString("Current Demand: "))
-                      .arg(current_demand)
-                      .arg(QString(10, ' ') + QString("Incoming: "))
-                      .arg(current_procurement));
+    // First line: Initial state
+    m_logger.info(formatRow(
+        QStringLiteral("Starting inventory:"), current_inventory,
+        QStringLiteral("Current demand:"), current_demand,
+        QStringLiteral("Incoming procurement:"), current_procurement));
 
-    // Procurement is incorporated at the beginning of the day/time slice.
+    // Process day's transactions
     current_inventory += current_procurement;
-
     qint64 sales{}, lost_sales{};
 
     if (current_inventory >= current_demand)
@@ -93,35 +124,36 @@ void qz::ChainSim::simulate_day(const PurchasePolicy &purchasePolicy, quint64 da
         current_inventory = 0;
     }
 
-    m_logger.info(QString("%1%2%3%4%5%6")
-                      .arg(QString(10, ' ') + QString("Sales: "))
-                      .arg(sales)
-                      .arg(QString(15, ' ') + QString("Lost Sales: "))
-                      .arg(lost_sales)
-                      .arg(QString(10, ' ') + QString("Ending Inventory: "))
-                      .arg(current_inventory));
+    // Second line: Transaction results
+    m_logger.info(formatRow(
+        QStringLiteral("Sales completed:"), sales,
+        QStringLiteral("Lost sales:"), lost_sales,
+        QStringLiteral("Ending inventory:"), current_inventory));
 
+    // Update records
     m_records[QStringLiteral("inventory_quantity")][day] = current_inventory;
     m_records[QStringLiteral("sale_quantity")][day] = sales;
     m_records[QStringLiteral("lost_sale_quantity")][day] = lost_sales;
 
+    // Purchase decision
     auto purchase_quantity = purchasePolicy.get_purchase(m_records, day);
 
     if (purchase_quantity > 0)
     {
         m_records[QStringLiteral("purchase_quantity")][day] = purchase_quantity;
-
         auto delivery_date = qMin(day + m_lead_time, m_simulation_length - 1);
         m_records[QStringLiteral("procurement_quantity")][delivery_date] += purchase_quantity;
 
-        m_logger.info(QStringLiteral("Using Purchase policy: %1").arg(purchasePolicy.name()));
-        m_logger.debug(QStringLiteral("Policy calculation: %1").arg(purchasePolicy.get_calculation_details(m_records, day)));
-        m_logger.warn(QStringLiteral("Purchasing {%1} units, will arrive on day {%2}.")
-                          .arg(purchase_quantity)
-                          .arg(delivery_date));
+        // Lines 3-5: Calculation details
+        m_logger.info(QString("\n%1Calculation Details:").arg(QString(leftMargin, ' ')));
+        QString details = purchasePolicy.get_calculation_details(m_records, day);
+        for (const QString &line : details.split('\n'))
+        {
+            m_logger.info(QString("%1%2").arg(QString(leftMargin + 2, ' ')).arg(line));
+        }
     }
 
-    m_logger.info(QString());
+    m_logger.info(QString()); // Empty line between days
 }
 
 qz::ChainSim::simulation_records_t qz::ChainSim::get_simulation_records() const
