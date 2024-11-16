e71fbc8714531062360b77069d828f927f07b0b7
Siraj Qazi <siraj.qazi@seeloz.com>
2024-11-16 05:48:10 +0500

    fix(eslint): disable unused-vars and no-explicit-any rules in chainsim-ui project feat(next.config.ts): add support for NEXT_PUBLIC_API_URL environment variable to be able to run app with a configurable API URL

--- chainsim-ui/next.config.ts
+++ chainsim-ui/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:47761/:path*'
+        destination: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/:path*` : 'http://localhost:47761/:path*'
       }
     ]
   },
